180859064c24815e72012031e33850c63aae1afb
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-10-03 08:22:57 +0200

    need to add header

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -10,4 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+#define FIXED_HPP
 
+#include <iostream>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const Fixed &src);
+		Fixed& operator=(const Fixed &src);
+		~Fixed();
+
+		int     getRawBits(void) const;
+		void    setRawBits(int const raw);
+
+	private:
+		int                 _fixedPointValue;
+		static const int    _fractionalBits = 8;
+};
+
+#endif
